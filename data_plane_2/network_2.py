3fc4b495385ca3f50967fe1c82c532043f6be741
Dan <daniel.bachler@comcast.net>
2019-10-23 14:00:17 -0600

    Working on v2

--- data_plane_2/network_2.py
+++ data_plane_2/network_2.py
@@ -38,9 +38,11 @@ class NetworkPacket:
     
     ##@param dst_addr: address of the destination host
     # @param data_S: packet payload
-    def __init__(self, dst_addr, data_S):
+    def __init__(self, dst_addr, data_S, isSegmentFlag = False, segmentNumber = 0):
         self.dst_addr = dst_addr
         self.data_S = data_S
+        self.segmentFlag = isSegmentFlag
+        self.segmentNumber = segmentNumber
         
     ## called when printing the object
     def __str__(self):
@@ -81,15 +83,34 @@ class Host:
     # @param dst_addr: destination address for the packet
     # @param data_S: data being transmitted to the network layer
     def udt_send(self, dst_addr, data_S):
-        p = NetworkPacket(dst_addr, data_S)
-        self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
-        print('%s: sending packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
+        # String to send can be done in one packet; make it so
+        if len(data_S) <= 30:
+            p = NetworkPacket(dst_addr, data_S)
+            self.out_intf_L[0].put(p.to_byte_S()) #send packets always enqueued successfully
+            print('%s: sending packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
+        # data string is too big; break into smaller packets
+        elif len(data_S) > 30:
+            segmentCount = 0
+            while len(data_S) > 30:
+                segmentCount += 1 # add to segment count
+                p = NetworkPacket(dst_addr, data_S[:29], True, segmentCount)    # make packet out of first 50 characters
+                self.out_intf_L[0].put(p.to_byte_S())       # and send it
+                print('%s: sending segment %i of packet "%s" on the out interface with mtu=%d' % (self, segmentCount, p, self.out_intf_L[0].mtu))
+                data_S = data_S[49:]    # remove sent string from data string
+            # data string no longer large than MTU; send final packet
+            p = NetworkPacket(dst_addr, data_S, True, segmentCount)
+            self.out_intf_L[0].put(p.to_byte_S()) # send final packet
+            print('%s: sending final segment of packet "%s" on the out interface with mtu=%d' % (self, p, self.out_intf_L[0].mtu))
         
     ## receive packet from the network layer
     def udt_receive(self):
         pkt_S = self.in_intf_L[0].get()
         if pkt_S is not None:
-            print('%s: received packet "%s" on the in interface' % (self, pkt_S))
+            # See if segmented
+            if pkt_S.segmentFlag is True:
+                pass
+            else:
+                print('%s: received packet "%s" on the in interface' % (self, pkt_S))
        
     ## thread target for the host to keep receiving data
     def run(self):
