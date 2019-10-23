82e51e595eaa1e08b28fa9eec58cf3f266d7f27d
DanielBachler <daniel.bachler@comcast.net>
2019-10-23 17:01:55 -0600

    made pycharm

--- data_plane_2/network_2.py
+++ data_plane_2/network_2.py
@@ -104,7 +104,7 @@ class Host:
                 p = NetworkPacket(dst_addr, data_S[:29], True, segmentCount)    # make packet out of first 50 characters
                 self.out_intf_L[0].put(p.to_byte_S())       # and send it
                 print('%s: sending segment %i of packet "%s" on the out interface with mtu=%d' % (self, segmentCount, p, self.out_intf_L[0].mtu))
-                data_S = data_S[49:]    # remove sent string from data string
+                data_S = data_S[29:]    # remove sent string from data string
             # data string no longer large than MTU; send final packet
             p = NetworkPacket(dst_addr, data_S, True, 0)
             self.out_intf_L[0].put(p.to_byte_S()) # send final packet
