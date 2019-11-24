7e94d98d1e4bcc3f55cc65b6af85a518a0e496d2
Logan-Shy <loganshy740@gmail.com>
2019-11-24 13:09:23 -0700

    Added reply message from host_2 in simulation.py

--- simulation_2.py
+++ simulation_2.py
@@ -59,6 +59,10 @@ if __name__ == '__main__':
     #send packet from host 1 to host 2
     host_1.udt_send('H2', 'MESSAGE_FROM_H1')
     sleep(simulation_time)
+
+    #send reply message from host 2 to host 1
+    # host_2.udt_send('H1', 'REPLY_MSG_FROM_H2')
+    # sleep(simulation_time)
     
     
     #join all threads
